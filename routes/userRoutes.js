3339887080f44c1a6cc3584d4cf4c2d027a153fe
ruhulcse <ruhul.cse7862@gmail.com>
2024-05-19 19:06:57 +0600

    authticaton api

---
+++ routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { protect, isShipper, isCarrier } = require("../middleware/auth");
+const {
+  Login,
+  Registration,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  getAllAgentUsers,
+} = require("../controllers/auth");
+
+router.route("/register").post(Registration);
+router.route("/login").post(Login);
+router.route("/users").get(protect, getAllUsers);
+router
+  .route("/user/:id")
+  .get(protect, getUserById)
+  .put(protect, updateUser)
+  .delete(protect, deleteUser);
+router.route("/agent-users").get(protect, getAllAgentUsers);
+
+module.exports = router;
