764a495b7d48ee27ada6ffa98523c47cb84de9d3
ruhulcse <ruhul.cse7862@gmail.com>
2024-07-10 21:53:24 +0600

    discount issue fixed

--- models/voucherModel.js
+++ models/voucherModel.js
@@ -4,7 +4,7 @@ const voucherSchema = new mongoose.Schema(
   {
     creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     store: { type: mongoose.Schema.Types.ObjectId, ref: "stores" },
-    discount: { type: Number, required: true },
+    discount: { type: Number, required: false },
     endDate: { type: Date, required: true },
     voucherCode: { type: String, required: true, unique: true },
     storeName: { type: String, required: true },
