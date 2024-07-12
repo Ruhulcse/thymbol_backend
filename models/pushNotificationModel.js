10a49d316fc1209357dab5728494fa962b4e2989
mmSohan <mesbahul.sohan@gmail.com>
2024-07-13 00:46:04 +0600

    store-create-validation&notificationtimestamp

--- models/pushNotificationModel.js
+++ models/pushNotificationModel.js
@@ -4,7 +4,7 @@ const pushNotificationSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: false },
     is_seen: {type: Number, default: 0}
-})
+},{ timestamps: true })
 
 const pushNotification = mongoose.model("pushNotifications", pushNotificationSchema);
 module.exports = pushNotification;
