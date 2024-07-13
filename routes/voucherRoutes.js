1bee9ebf3613818c37bfcebd12e0922d24009b38
mmSohan <mesbahul.sohan@gmail.com>
2024-07-13 23:25:59 +0600

    voucher-notification-api-update

--- routes/voucherRoutes.js
+++ routes/voucherRoutes.js
@@ -6,6 +6,8 @@ const {
   clippedDetails,
   getvoucherByStore,
   getSingleVoucher,
+  deleteVoucher,
+  updateVoucherDetails
 } = require("../controllers/voucher");
 const router = express.Router();
 const { protect } = require("../middleware/auth");
@@ -15,5 +17,7 @@ router.route("/voucher/:id").get(getSingleVoucher);
 router.route("/voucherbystore/:id").get(getvoucherByStore);
 router.route("/voucher/clipped-for-later").post(protect, clippedVoucher);
 router.route("/voucher/clipped-details/:id").get(protect, clippedDetails);
+router.route("/voucher/:id").delete(protect, deleteVoucher)
+router.route("/voucher/:id").patch(protect, updateVoucherDetails);
 
 module.exports = router;
