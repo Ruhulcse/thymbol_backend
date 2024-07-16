792cc76790faa8a04aff714631210f13bee7dad2
ruhulcse <ruhul.cse7862@gmail.com>
2024-07-17 01:06:04 +0600

    updated

--- routes/pushNotificationRoutes.js
+++ routes/pushNotificationRoutes.js
@@ -3,20 +3,22 @@ const router = express.Router();
 const { protect, isShipper, isCarrier } = require("../middleware/auth");
 
 const {
-    createPushNotification,
-    getPushNotification,
-    deletePushNotification,
-    getAllNotification,
-    updatePushNotification
-
-} = require("../controllers/pushNotification")
+  createPushNotification,
+  getPushNotification,
+  deletePushNotification,
+  getAllNotification,
+  updatePushNotification,
+  GetMedicalData,
+} = require("../controllers/pushNotification");
 
 router.route("/push-notificaton/create").post(protect, createPushNotification);
 router.route("/push-notificaton/all").get(protect, getAllNotification);
-router.route("/push-notificaton/delete").delete(protect, deletePushNotification);
 router
-    .route("/push-notificaton/:id")
-    .get(protect, getPushNotification)
-    .patch(protect, updatePushNotification)
+  .route("/push-notificaton/delete")
+  .delete(protect, deletePushNotification);
+router
+  .route("/push-notificaton/:id")
+  .get(protect, getPushNotification)
+  .patch(protect, updatePushNotification);
 
 module.exports = router;
